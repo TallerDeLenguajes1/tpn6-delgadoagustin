b163fa8c2cca5aa3ac5702c9eda3d7669cce3c27
Agustin Delgado <delgadoagustin147@gmail.com>
2020-05-20 15:12:53 -0300

    Desarrolo 1)

--- tp6/tp6/Program.cs
+++ tp6/tp6/Program.cs
@@ -6,7 +6,35 @@ namespace tp6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcion, a, b;
+            Console.Write("Ingresa un numero a invertir: ");
+            int invertir = Convert.ToInt32(Console.ReadLine());
+
+            while (invertir > 0){
+                Console.Write(invertir % 10);
+                invertir /= 10;
+            }
+
+            Console.WriteLine("Welcome!!\n");
+            do{
+                Console.WriteLine("Opciones");
+                Console.WriteLine("1) Sumar.");
+                Console.WriteLine("2) Restar.");
+                Console.WriteLine("3) Multiplicar.");
+                Console.WriteLine("4) Dividir.");
+                Console.WriteLine("0) Salir.");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }while(opcion!=0);
         }
     }
 }
